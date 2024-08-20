c54b07054699a8620368b559427872a3b5e03aa7
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-08-20 22:53:28 +0200

    Remove lint

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const addInherited = require('jsdoc/augment').addInherited; // eslint-disable-li
 const config = env.conf.typescript;
 if (!config) {
   throw new Error(
-    'Configuration "typescript" for jsdoc-plugin-typescript missing.'
+    'Configuration "typescript" for jsdoc-plugin-typescript missing.',
   );
 }
 if (!('moduleRoot' in config)) {
   throw new Error(
-    'Configuration "typescript.moduleRoot" for jsdoc-plugin-typescript missing.'
+    'Configuration "typescript.moduleRoot" for jsdoc-plugin-typescript missing.',
   );
 }
 const moduleRoot = config.moduleRoot;
@@ -21,7 +21,7 @@ if (!fs.existsSync(moduleRootAbsolute)) {
   throw new Error(
     'Directory "' +
       moduleRootAbsolute +
-      '" does not exist. Check the "typescript.moduleRoot" config option for jsdoc-plugin-typescript'
+      '" does not exist. Check the "typescript.moduleRoot" config option for jsdoc-plugin-typescript',
   );
 }
 
@@ -47,7 +47,7 @@ function getModuleInfo(moduleId, extension, parser) {
       const absolutePath = path.join(
         process.cwd(),
         moduleRoot,
-        moduleId + extension
+        moduleId + extension,
       );
       if (!fs.existsSync(absolutePath)) {
         return null;
@@ -135,7 +135,7 @@ exports.defineTags = function (dictionary) {
         }
         throw new Error("Missing closing '}'");
       };
-    }
+    },
   );
 };
 
@@ -229,10 +229,10 @@ exports.astNodeVisitor = {
             if (
               leadingComments.length === 0 ||
               (leadingComments[leadingComments.length - 1].value.indexOf(
-                '@classdesc'
+                '@classdesc',
               ) === -1 &&
                 noClassdescRegEx.test(
-                  leadingComments[leadingComments.length - 1].value
+                  leadingComments[leadingComments.length - 1].value,
                 ))
             ) {
               // Create a suitable comment node if we don't have one on the class yet
@@ -250,7 +250,7 @@ exports.astNodeVisitor = {
             if (node.superClass) {
               // Remove the `@extends` tag because JSDoc does not does not handle generic type. (`@extends {Base<Type>}`)
               const extendsIndex = lines.findIndex((line) =>
-                line.includes('@extends')
+                line.includes('@extends'),
               );
               if (extendsIndex !== -1) {
                 lines.splice(extendsIndex, 1);
@@ -262,7 +262,7 @@ exports.astNodeVisitor = {
               if (identifier) {
                 const absolutePath = path.resolve(
                   path.dirname(currentSourceName),
-                  identifier.value
+                  identifier.value,
                 );
                 // default to js extension since .js extention is assumed implicitly
                 const extension = getExtension(absolutePath);
@@ -295,7 +295,7 @@ exports.astNodeVisitor = {
           // Replace typeof Foo with Class<Foo>
           comment.value = comment.value.replace(
             /typeof ([^,\|\}\>]*)([,\|\}\>])/g,
-            'Class<$1>$2'
+            'Class<$1>$2',
           );
 
           // Remove `@override` annotations to avoid JSDoc breaking the inheritance chain
@@ -323,7 +323,7 @@ exports.astNodeVisitor = {
                 if (replaceAttempt > 100) {
                   // infinite loop protection
                   throw new Error(
-                    `Invalid docstring ${comment.value} in ${currentSourceName}.`
+                    `Invalid docstring ${comment.value} in ${currentSourceName}.`,
                   );
                 }
               } else {
@@ -332,7 +332,7 @@ exports.astNodeVisitor = {
               lastImportPath = importExpression;
               const rel = path.resolve(
                 path.dirname(currentSourceName),
-                importSource
+                importSource,
               );
               // default to js extension since .js extention is assumed implicitly
               const extension = getExtension(rel);
@@ -356,7 +356,7 @@ exports.astNodeVisitor = {
             if (replacement) {
               comment.value = comment.value.replace(
                 importExpression,
-                replacement + remainder
+                replacement + remainder,
               );
             }
           }
@@ -377,13 +377,13 @@ exports.astNodeVisitor = {
           Object.keys(identifiers).forEach((key) => {
             const eventRegex = new RegExp(
               `@(event |fires )${key}([^A-Za-z])`,
-              'g'
+              'g',
             );
             replace(eventRegex);
 
             const typeRegex = new RegExp(
               `@(.*[{<|,(!?:]\\s*)${key}([^A-Za-z].*?\}|\})`,
-              'g'
+              'g',
             );
             replace(typeRegex);
 
@@ -392,7 +392,7 @@ exports.astNodeVisitor = {
                 const identifier = identifiers[key];
                 const absolutePath = path.resolve(
                   path.dirname(currentSourceName),
-                  identifier.value
+                  identifier.value,
                 );
                 // default to js extension since .js extention is assumed implicitly
                 const extension = getExtension(absolutePath);
@@ -408,11 +408,11 @@ exports.astNodeVisitor = {
                     : getDelimiter(moduleId, exportName, parser);
                   const replacement = `module:${moduleId.replace(
                     slashRegEx,
-                    '/'
+                    '/',
                   )}${exportName ? delimiter + exportName : ''}`;
                   comment.value = comment.value.replace(
                     regex,
-                    '@$1' + replacement + '$2'
+                    '@$1' + replacement + '$2',
                   );
                 }
               }
